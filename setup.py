2bd541b9af0d5003a19b1d6fa7e7bdbe29845447
Darwin Monroy <contact@darwinmonroy.com>
2016-02-06 14:16:20 -0600

    project bootstrap

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setup_params = dict(
+    name='chilero_pg',
+    use_scm_version=True,
+    namespace_packages=['chilero'],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    url='https://github.com/dmonroy/chilero.pg',
+    author='Darwin Monroy',
+    author_email='contact@darwinmonroy.com',
+    description='PostgreSQL utilities for chilero',
+    install_requires=[
+        'aiopg',
+        'chilero>=0.3.2'
+    ],
+    setup_requires=[
+        'setuptools_scm',
+    ],
+)
+
+
+if __name__ == '__main__':
+    setuptools.setup(**setup_params)
