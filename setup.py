0237b099ab879f1492039e268de6e78624b79047
Darwin Monroy <contact@darwinmonroy.com>
2016-03-15 20:29:53 -0600

    pinning chilero>=0.3.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup_params = dict(
     description='PostgreSQL utilities for chilero',
     install_requires=[
         'aiopg',
-        'chilero>=0.3.5'
+        'chilero>=0.3.6'
     ],
     setup_requires=[
         'setuptools_scm',
